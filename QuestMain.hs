1547f59bd6d4d65365441804a85f62e69ecf70c7
Dzianis Dashkevich <dskecse@gmail.com>
2014-08-16 21:25:05 +0300

    Avoid messing up with string case
    Use `toUpper` function from `Data.Char` module, use `map` to iterate over string characters

--- QuestMain.hs
+++ QuestMain.hs
@@ -1,5 +1,10 @@
+import Data.Char
+
+upperCaseString :: String -> String
+upperCaseString str = map toUpper str
+
 describeLocation :: String -> String
-describeLocation locName = case locName of
-  "Home"       -> "You are standing in the middle of the room at the wooden table."
-  "FriendYard" -> "You are standing in front of the night garden behind the small wooden fence."
+describeLocation locName = case (upperCaseString locName) of
+  "HOME"       -> "You are standing in the middle of the room at the wooden table."
+  "FRIENDYARD" -> "You are standing in front of the night garden behind the small wooden fence."
   otherwise    -> "Unknown location."
