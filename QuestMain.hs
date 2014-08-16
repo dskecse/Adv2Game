befa0c39ffd51c6c8ec8f6513aad7167871fdba9
Dzianis Dashkevich <dskecse@gmail.com>
2014-08-16 21:22:17 +0300

    Change parameter type to String

--- QuestMain.hs
+++ QuestMain.hs
@@ -1,6 +1,5 @@
-describeLocation :: Integer -> String
-describeLocation locNumber = case locNumber of
-  1 -> "You are standing in the middle of the room at the wooden table."
-  2 -> "You are standing in front of the night garden behind the small wooden fence."
-  -- Descriptions of other locations go here.
-  otherwise -> "Unknown location."
+describeLocation :: String -> String
+describeLocation locName = case locName of
+  "Home"       -> "You are standing in the middle of the room at the wooden table."
+  "FriendYard" -> "You are standing in front of the night garden behind the small wooden fence."
+  otherwise    -> "Unknown location."
