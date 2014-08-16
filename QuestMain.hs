dffc39cd99245ef12c56b8d0ffe3bf91aa39daff
Dzianis Dashkevich <dskecse@gmail.com>
2014-08-17 00:20:51 +0300

    Show location name before its description

--- QuestMain.hs
+++ QuestMain.hs
@@ -25,8 +25,9 @@ data Action = Look
     deriving (Eq, Show)
 
 describeLocation :: Location -> String
-describeLocation loc = case loc of
-  Home       -> "You are standing in the middle of the room at the wooden table."
-  FriendYard -> "You are standing in front of the night garden behind the small wooden fence."
-  Garden     -> "You are in the garden. Garden looks very well: clean, tonsured, cool and wet."
-  otherwise  -> "No description available for location with name " ++ show loc ++ "."
+describeLocation loc = show loc ++ "\n" ++
+  case loc of
+    Home       -> "You are standing in the middle of the room at the wooden table."
+    FriendYard -> "You are standing in front of the night garden behind the small wooden fence."
+    Garden     -> "You are in the garden. Garden looks very well: clean, tonsured, cool and wet."
+    otherwise  -> "No description available for location with name " ++ show loc ++ "."
