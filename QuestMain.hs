3a562e658e6072a351c9e3f0d900578ca68560ff
Dzianis Dashkevich <dskecse@gmail.com>
2014-08-17 15:27:58 +0300

    Handle Look action in `run` function

--- QuestMain.hs
+++ QuestMain.hs
@@ -9,6 +9,9 @@ run curLoc = do
   x <- getLine
   case (convertStringToAction x) of
     Quit             -> putStrLn "Be seen you..."
+    Look             -> do
+      putStrLn (describeLocation curLoc)
+      run curLoc
     Go dir           -> do
       putStrLn ("\nYou're going to " ++ show dir ++ ".\n")
       run (walk curLoc dir)
