30a25f0648776708ff142f8c14fc55fd81a9f091
Dzianis Dashkevich <dskecse@gmail.com>
2014-08-17 15:06:01 +0300

    Add a new GameAction module

--- QuestMain.hs
+++ QuestMain.hs
@@ -1,31 +1,7 @@
 module Main where
 
 import Types
-
-describeLocation :: Location -> String
-describeLocation loc = show loc ++ "\n" ++
-  case loc of
-    Home       -> "You are standing in the middle of the room at the wooden table."
-    FriendYard -> "You are standing in front of the night garden behind the small wooden fence."
-    Garden     -> "You are in the garden. Garden looks very well: clean, tonsured, cool and wet."
-    otherwise  -> "No description available for location with name " ++ show loc ++ "."
-
-evalAction :: Action -> String
-evalAction act = "Action: " ++ show act ++ "!"
-
-convertStringToAction :: String -> Action
-convertStringToAction str = case reads str of
-  [(x, _)] -> x
-  _        -> Quit
-
-walk :: Location -> Direction -> Location
-walk Home North       = Garden
-walk Home South       = FriendYard
-walk FriendYard North = Home
-walk FriendYard South = Garden
-walk Garden North     = FriendYard
-walk Garden South     = Home
-walk curLoc _         = curLoc
+import GameAction
 
 run curLoc = do
   putStrLn (describeLocation curLoc)
